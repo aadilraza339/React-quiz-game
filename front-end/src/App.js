384b98bd3efd4e0fc0b3b34513376b3d2146fd78
Aadil Raza <aadil18@navgurukul.org>
2021-05-28 20:56:39 +0530

    frontend of quiz game

---
+++ front-end/src/App.js
@@ -0,0 +1,15 @@
+
+// import './App.css';
+import React, {Component} from 'react';
+import Userinput from './Components/user_input';
+class Quiz extends Component {
+  render(){
+    return (
+      <div>
+        <Userinput />
+      </div>
+    );
+  }
+}
+
+export default Quiz;
