be58cf94fdf64770798d509e3b1c8a2b3a068d46
aadilraza339 <aadil18@navgurukul.org>
2021-06-02 08:57:29 +0530

    Update users.js

--- express_server/routers/users.js
+++ express_server/routers/users.js
@@ -6,9 +6,6 @@ router.post('/name', userControllers.inser_candidat);
 
 router.get('/get', userControllers.get_detail);
 
-// router.get('/Score', userControllers.get_score);
-
-
 module.exports = router
 
 
